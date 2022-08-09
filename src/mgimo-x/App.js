ebfdedab5b9f557857cbf54dd919950f26b24c52
s1quell <95284894+s1quell@users.noreply.github.com>
2022-08-09 05:31:37 +0300

    Установка и настройка базовых пакетов разработки

--- src/mgimo-x/App.js
+++ src/mgimo-x/App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// Importing libraries, such as:
+// Styled Components - css styling react components
+// Expo Fonts - add custom app font
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import styled from "styled-components/native";
+
+// Importing font's settings
+import * as font from "./settings/fonts";
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+    return (
+        <View style={styles.container}>
+            <Text style={{ fontSize: font.HEADER }}>
+                Open up App.js to start working on your app!
+            </Text>
+            <StatusBar style="auto" />
+        </View>
+    );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: "#fff",
+        alignItems: "center",
+        justifyContent: "center",
+    },
 });
---
+++ src/mgimo-x/settings/fonts.js
@@ -0,0 +1,9 @@
+// #region Размер шрифтов заголовков
+
+export const HEADER = 16;
+export const HEADER_ARTICLE = 16;
+export const HEADER_2 = 16;
+
+// #endregion
+
+export const REGULAR_TEXT = 14;
